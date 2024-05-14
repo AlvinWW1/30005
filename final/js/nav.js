02921e95449d897017ec6dd88294ca81b29d22ea
Wenjie Wang <165708944+AlvinWW1@users.noreply.github.com>
2024-05-14 00:26:07 -0500

    Update nav.js

--- final/js/nav.js
+++ final/js/nav.js
@@ -1,1 +1,1 @@
-$(function () {$("#nav-placeholder").load("https://github.com/AlvinWW1/alvinww1.github.io/tree/main/final/nav.html");});
+$(function () {$("#nav-placeholder").load("alvinww1.github.io/final/nav.html");});
